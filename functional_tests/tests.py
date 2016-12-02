4823e7d7a74543c16afd93c9d9a19ba853087be8
Alex Turner <aturner@gmail.com>
2016-12-02 23:25:06 +1100

    make function_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Chrome()
@@ -20,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
 
 		# Edith has heard about a cool new online to-do app. She goes
 		# to chekc out its homepage
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
